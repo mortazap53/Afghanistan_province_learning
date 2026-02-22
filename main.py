703d1a89c706585ce7b848e5394b2df16182a594
R1stAMOR <mortazap53@gmail.com>
2026-02-22 15:48:16 +0430

    start coding

---
+++ main.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+import pandas
+
+
+screen = Screen()
+Turtle = Turtle()
+screen.setup(width=800, height=600)
+image = "AFG map.gif"
+screen.addshape(image)
+Turtle.shape(image)
+
