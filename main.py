7e4af9c18801f459000a7646a1e7d14a5b222aa8
R1stAMOR <mortazap53@gmail.com>
2026-02-22 17:14:43 +0430

    making the loop and making an exit option, also tracking the remaining provinces

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from turtle import Turtle, Screen
 from writing_name import Write
-
 import pandas
-
 
 screen = Screen()
 Turtle = Turtle()
@@ -15,15 +13,20 @@ data = pandas.read_csv("Afghanistan_provinces.csv")
 Provinces = data.provinces.tolist()
 
 guessed_provinces = []
-guessing_province = screen.textinput(f"{len(guessed_provinces) + 1}/{len(Provinces)} Guess a province",
-                                    "Write name of a province")
-if guessing_province in Provinces not in guessed_provinces:
-    guessed_provinces.append(guessing_province)
-    row = data[data.provinces == guessing_province]
-    xcor = row.x.item()
-    ycor = row.y.item()
-    Write(guessing_province, (xcor, ycor))
+while len(guessed_provinces) < len(Provinces):
+    guessing_province = screen.textinput(f"{len(guessed_provinces) + 1}/{len(Provinces)} Guess a province",
+                                        "Write name of a province").title()
+    if guessing_province == "Exit":
+        break
+    if guessing_province in Provinces not in guessed_provinces:
+        guessed_provinces.append(guessing_province)
+        row = data[data.provinces == guessing_province]
+        xcor = row.x.item()
+        ycor = row.y.item()
+        Write(guessing_province, (xcor, ycor))
 
-
-
-screen.exitonclick()
+remaining_provinces = []
+for province in Provinces:
+    if province not in guessed_provinces:
+        remaining_provinces.append(province)
+print(f"Here is all the remaining provinces:{remaining_provinces}")
--- writing_name.py
+++ writing_name.py
@@ -5,6 +5,7 @@ Font = ("Arial", 8, "bold")
 class Write(Turtle):
     def __init__(self, province, position):
         super().__init__()
-        self.shape("turtle")
+        self.hideturtle()
+        self.penup()
         self.goto(position)
-        self.write(province, font=Font)
+        self.write(arg=province, font=Font)
